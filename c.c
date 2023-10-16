2525e363645fd3fd393f31bedda3e1c935eb66c6
dEIOnTomsoN <125672061+dEIOnTomsoN@users.noreply.github.com>
2023-10-16 16:09:49 +0530

    Update c.c

--- c.c
+++ c.c
@@ -14,4 +14,36 @@ public class SmallestNumberInAnArray {
          }
       }
       System.out.println("2nd Smallest element of the array is:: "+array[0]);
+public class Prime
+{
+    public static void main(String args[])
+    {
+         int s1, s2, s3, flag = 0, i, j;
+         Scanner s = new Scanner(System.in);
+         System.out.println ("Enter the lower limit :"); 
+         s1 = s.nextInt();
+         System.out.println ("Enter the upper limit :"); 
+         s2 = s.nextInt();
+         System.out.println ("The prime numbers in between the entered limits are :");
+         for(i = s1; i <= s2; i++)
+         {
+             for( j = 2; j < i; j++)
+             {
+                 if(i % j == 0)
+                 {
+                     flag = 0;
+                     break;
+                 }
+                 else
+                 {
+                     flag = 1;
+                 }
+             }
+             if(flag == 1)
+             {
+                 System.out.println(i);
+             }
+         }
+    }
+}
    
